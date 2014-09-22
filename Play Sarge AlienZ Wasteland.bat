87daef42f38abccb9b797ac8f547f3bf22155db2
Frank <iaretechnician@gmail.com>
2014-09-21 21:37:33 -0400

    initial upload

---
+++ Play Sarge AlienZ Wasteland.bat
@@ -0,0 +1,1 @@
+start Expansion\beta\arma2oa.exe -mod=@alienZ -beta=Expansion\beta;Expansion\beta\Expansion -nosplash -world=empty
---
+++ Start Sarge AlienZ Wasteland Server.bat
@@ -0,0 +1,1 @@
+"Expansion\beta\arma2oaserver.exe" -port=2302 -mod=@alienZ;@inidb "-config=AlienZSargeWasteland\config.cfg" "-cfg=AlienZSargeWasteland\basic.cfg" "-profiles=AlienZSargeWasteland" -name=AlienZSargeWasteland -cpuCount=2 -noSound -exThreads=7 -noPause
